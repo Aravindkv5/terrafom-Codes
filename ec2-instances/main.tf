3547ebb0678fe659f84ad345cb495814b60dc7a7
Aravindkv5 <98680881+Aravindkv5@users.noreply.github.com>
2022-11-09 17:45:36 +0530

    main.tf file
    have all the main constraints

---
+++ ec2-instances/main.tf
@@ -0,0 +1,19 @@
+resource "aws_instance" "terraform" {
+    ami = var.ami
+    instance_type = var.instance_type
+    key_name = var.key_name
+    count = var.instance_count[0]
+    iam_instance_profile = var.iam_instance_profile[0]
+    security_groups = var.security_groups
+    associate_public_ip_address = var.associate_public_ip_address
+    availability_zone = var.availability_zone
+    hibernation = var.hibernation
+    subnet_id = var.subnet_id
+    tags = {
+        name = "terraform testing" 
+    }
+    user_data = file("userdata.tpl")
+      
+}
+#name was empty in created ec2 instance and output was instance ID
+#need to check on how to provide keypair
